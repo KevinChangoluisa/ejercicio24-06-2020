f4880a66c32ff4408eff6c0cf58238b184a02b5f
KevinChangoluisa <kchangoluisa@est.ups.edu.ec>
2020-07-09 23:44:51 -0500

    primer commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+require('./config/config')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+app.get('/usuario', (req, res) => {
+    res.json("Get Usuario")
+});
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+    if (body.nombre == undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es necesario"
+        });
+    } else {
+        res.json({
+            persona: body
+        })
+    }
+});
+app.delete('/usuario', (req, res) => {
+    res.json("Delete Usuario")
+});
+app.put('/usuario', (req, res) => {
+    res.json("Put Usuario")
+});
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando el puerto ", process.env.PORT);
+});
